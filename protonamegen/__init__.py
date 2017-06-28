027e455646867663d9286dc74bb6e5bea6ebc861
Kevin Wierman <kwierman@gmail.com>
2017-06-28 15:12:39 -0500

    Adding definition to this whole shebang

---
+++ protonamegen/__init__.py
@@ -0,0 +1,37 @@
+import nltk
+import random
+
+__nouns__      = None
+__adjectives__ = None
+
+
+def __initialize__():
+  global __nouns__
+  global __adjectives__
+  wn = nltk.wordnet.wordnet
+  __nouns__ = []
+  __adjectives__ = []
+  for synset in wn.all_synsets('n'):
+    word = synset.name().split('.n')[0]
+    if not "_" in word or '-' in word:
+      __nouns__.append(word)
+  for synset in wn.all_synsets('a'):
+    word = synset.name().split('.a')[0]
+    if not "_" in word or '-' in word:
+      __adjectives__.append(word)
+
+
+def gen_noun():
+  global __nouns__
+  if __nouns__ is None:
+    __initialize__()
+  return random.choice(__nouns__)
+
+def gen_adjective():
+  global __adjectives__
+  if __adjectives__ is None:
+    __initialize__()
+  return random.choice(__adjectives__)
+
+def gen_name():
+  return gen_adjective() + "_" + gen_noun()
